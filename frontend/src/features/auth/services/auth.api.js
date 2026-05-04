d81edd540902435b39a18a7e76a92095bfe0299f
Junaid Arshad <junaidarshad647@gmail.com>
2026-05-04 13:15:59 +0500

    fix the endpoint

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://ai-resume-analyzer-ats-hs1f.vercel.app/",
   withCredentials: true,
 });
 export async function register({ username, email, password }) {
