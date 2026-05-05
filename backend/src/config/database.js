cf51c69f070ccd2acf5a76f3923b53fa07bac722
Junaid Arshad <junaidarshad647@gmail.com>
2026-05-05 11:36:46 +0500

    Revert "fix the deployment issues"
    This reverts commit e1a4ae7523d3716a07623b713b1b8d454824f030.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,30 +1,11 @@
 const mongoose = require("mongoose");
 
-let cachedConnectionPromise = null;
-
 async function connectToDB() {
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI is not configured");
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to Database");
+  } catch (error) {
+    console.log(error);
   }
-
-  if (mongoose.connection.readyState === 1) {
-    return mongoose.connection;
-  }
-
-  if (!cachedConnectionPromise) {
-    cachedConnectionPromise = mongoose
-      .connect(process.env.MONGO_URI)
-      .then((connection) => {
-        console.log("Connected to Database");
-        return connection;
-      })
-      .catch((error) => {
-        cachedConnectionPromise = null;
-        throw error;
-      });
-  }
-
-  return cachedConnectionPromise;
 }
-
 module.exports = connectToDB;
