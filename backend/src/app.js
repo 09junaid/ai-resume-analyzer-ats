224a98a8adfffb0bfe23d20cd5c6c9f4f68c6551
Junaid Arshad <junaidarshad647@gmail.com>
2026-05-05 12:14:39 +0500

    fix the repo

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://ai-resume-analyzer-ats-nine.vercel.app/",
+    origin: "http://localhost:5173/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
--- frontend/src/features/interview/services/interview.api.js
+++ frontend/src/features/interview/services/interview.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://ai-resume-analyzer-ats-hs1f.vercel.app/",
+  baseURL: "http://localhost:3000",
   withCredentials: true,
 });
 
